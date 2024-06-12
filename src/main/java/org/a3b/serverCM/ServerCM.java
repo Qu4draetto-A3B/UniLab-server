5199d3d6d678aec8b04fcd91796ea00d918b3338
Iuri <iuricube@gmail.com>
2024-06-12 13:57:18 +0200

    Conversione RMI, database

--- src/main/java/org/a3b/serverCM/ServerCM.java
+++ src/main/java/org/a3b/serverCM/ServerCM.java
@@ -1,32 +1,46 @@
 package org.a3b.serverCM;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.Data;
 import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
 
 @Log4j2
 public class ServerCM {
-	private static final int N_THREADS = 64;
-	private static final Thread MAIN_THREAD = Thread.currentThread();
-
-	private static ServerPool pool = new ServerPool(N_THREADS);
+	public static Connection db;
+	public static Registry registry;
 
 	public static void main(String[] args) {
-		// Stop when it receives CTRL-c
-		Runtime.getRuntime().addShutdownHook(new Thread(MAIN_THREAD::interrupt));
-
-		log.info("Starting server");
-		try (ServerSocket server = new ServerSocket(8080)) {
-			while (!Thread.interrupted()) {
-				log.trace("Waiting connection");
-				pool.execute(server.accept());
-			}
-		} catch (IOException e) {
-			log.error("Stopping server due to IOException");
-			log.error(e.toString());
-		} finally {
-			pool.close();
+		try {
+			init();
+		} catch (Exception e) {
+			e.printStackTrace();
+			//log.error(e);
 		}
+	}
+
+	private static void init() throws Exception {
+		log.trace("Initializing ServerCM");
+
+		Dotenv env = Dotenv.configure().filename("postgres.env").ignoreIfMissing().load();
+
+		String url = "jdbc:postgresql://localhost/dbCM";
+		Properties props = new Properties();
+		props.setProperty("user", "postgres");
+		props.setProperty("password", env.get("POSTGRES_PASSWORD"));
+		db = DriverManager.getConnection(url, props);
+
+		registry = LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
+		registry.rebind("CM", new UnicastRemoteObject() {});
+
+		log.trace("ServerCM initialized");
 	}
 }
